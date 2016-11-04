5600edeec99f2bc201f7501fc8a4d07d4adeee91
wond3rwise <wond3rwise@gmail.com>
2016-11-04 19:50:59 +0200

    Add files via upload

---
+++ 06.-Loops/16. Trailing 0 in N!/Trailing0inN!.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Trailing0inNFact
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int addFactor = 0;
+        int devide = 0;
+        int counter = 1;
+
+        while (true)
+        {
+            devide = (int)Math.Pow(5, counter);
+            addFactor += n / devide;
+            if ((n / devide) < 1)
+            {
+                break;
+            }
+            counter++;
+        }
+        Console.WriteLine(addFactor);
+    }
+}
---
+++ 06.-Loops/17. Spiral Matrix/SpiralMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class SpiralMatrix
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] array = new int[n, n];
+        int element = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n - i; j++)
+            {
+                element++;
+                array[i, j] = element;
+            }
+            for (int k = i; k < n - i; k++)
+            {
+                array[k, (n - i - 1)] = element;
+                element++;
+            }
+            element--;
+            for (int l = (n - i - 1); l > i; l--)
+            {
+                array[(n - i - 1), l] = element;
+                element++;
+            }
+            for (int m = n - i - 1; m > i; m--)
+            {
+                array[m, i] = element;
+                element++;
+            }
+            element--;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
