2a76a38b9a2434a26bd8afcf2ff58c5ffea2d320
wond3rwise <wond3rwise@gmail.com>
2016-11-04 19:35:19 +0200

    Add files via upload

---
+++ 04.-Console-In-and-Out/11. Interval/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Intervals
+{
+    public static void Main()
+    {
+        long start = long.Parse(Console.ReadLine());
+        long end = long.Parse(Console.ReadLine());
+        int count = 0;
+        for (long i = start+1; i <= end-1; i++)
+        {
+            if (i % 5 == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
---
+++ 04.-Console-In-and-Out/Sum of 3 numbers/SumOf3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class SumOf3
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a=");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("enter b=");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine("enter c=");
+        double c = double.Parse(Console.ReadLine());
+        Console.WriteLine("a+b+c={0}", a+b+c);
+    }
+     
+}
