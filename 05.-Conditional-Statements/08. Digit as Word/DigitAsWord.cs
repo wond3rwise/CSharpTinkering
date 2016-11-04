cad0ff78ae7188af856d9adf860764a42d604d04
wond3rwise <wond3rwise@gmail.com>
2016-11-04 19:43:25 +0200

    Add files via upload

---
+++ 05.-Conditional-Statements/08. Digit as Word/DigitAsWord.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+class DigitAsWord
+{
+    static void Main()
+    {
+        string inputValue = Console.ReadLine();
+        double digit;
+        bool parseSuccess = double.TryParse(inputValue, out digit);
+        if (parseSuccess)
+        {
+            if (digit % 1 == 0)
+            {
+                switch (inputValue)
+                {
+                    case "0": Console.WriteLine("zero"); break;
+                    case "1": Console.WriteLine("one"); break;
+                    case "2": Console.WriteLine("two"); break;
+                    case "3": Console.WriteLine("three"); break;
+                    case "4": Console.WriteLine("four"); break;
+                    case "5": Console.WriteLine("five"); break;
+                    case "6": Console.WriteLine("six"); break;
+                    case "7": Console.WriteLine("seven"); break;
+                    case "8": Console.WriteLine("eight"); break;
+                    case "9": Console.WriteLine("nine"); break;
+                    default:
+                        if ((digit < 0) || (digit > 9))
+                        {
+                            Console.WriteLine("not a digit");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("not a digit");
+            }
+        }
+        else
+        {
+            Console.WriteLine("not a digit");
+        }
+
+    }
+}
---
+++ 05.-Conditional-Statements/09. Int, Double and String/IntDoubleString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PlayWithIntDoubleAndString
+{
+    class PlayWithDoubleAndString
+    {
+        static void Main()
+        {
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "integer":
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a + 1);
+                    break;
+                case "real":
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0:F2}", b + 1.00);
+                    break;
+                case "text":
+                    string c = Console.ReadLine();
+                    Console.WriteLine(c + "*");
+                    break;
+                default: Console.WriteLine("Not correct choice!"); break;
+            }
+        }
+    }
+}
