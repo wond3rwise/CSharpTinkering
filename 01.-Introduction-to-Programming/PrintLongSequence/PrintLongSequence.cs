46026cca18f35f9ad27d57d3c2aa51a55303b2f1
wond3rwise <wond3rwise@gmail.com>
2016-11-04 19:02:55 +0200

    Add files via upload

---
+++ 01.-Introduction-to-Programming/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace PrintLongSequence
+{
+    class PrintLongSequence
+    {
+        static void Main()
+        {
+            for (int i = 2; i < 1002; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(-i);
+
+                }
+            }
+        }
+    }
+}
